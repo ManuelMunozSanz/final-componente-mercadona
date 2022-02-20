68e1d9a01e754baad786b526461988a48ebe4635
Osman Mehmed <osman@devanddel.com>
2022-02-20 21:32:45 +0100

    Subiendo bloques funcionales 4 y 5

--- componente.js
+++ componente.js
@@ -1,11 +1,20 @@
 var contador = 1;
+var precioTotal = 0;
 
 var unidad = document.getElementById("unidad");
+var precioTotal = document.getElementById("precioTotal");
+var precioArticulo = document.getElementById("precioArticulo");
+
+precioTotal.textContent = precioArticulo.textContent;
+
+/* console.log(typeof(parseInt(precioArticulo.textContent)); */
 
 function addUnidad(){
     console.log("addUnidad");
     contador++;
     unidad.textContent = contador;
+    precioTotal.textContent = contador * parseFloat(precioArticulo.textContent);
+    /* console.log(parseFloat(precioArticulo.textContent)); */
 }
 
 function remUnidad(){
@@ -13,7 +22,15 @@ function remUnidad(){
     if(contador > 1){
         contador--;
         unidad.textContent = contador;
+        precioTotal.textContent = contador * parseFloat(precioArticulo.textContent);
     }
     
+}
+
+function vaciarCarrito(){
+    console.log("vaciarCarrito");
+    precioTotal.textContent = 0;
+    unidad.textContent = 0;
+    contador = 0;
 }
 
